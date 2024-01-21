933f51c4149b3ed4563525d28dcde72570ed116f
adibmansuri511 <adibmansuri.developer@gmail.com>
2024-01-21 19:00:58 +0530

    Add new style & some changes in main.js file.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,16 @@
-const randomNumber = () => {
+// program to get the URL
+
+// Get button element and add click event listener on it and assign to element variable.
+const myBtn = document.getElementById("myBtn");
+
+myBtn.addEventListener("click", randomNumber);
+
+
+function randomNumber(e) {
+    e.preventDefault();
+
+    // Show the result on this #h5 id.
+    let result = document.querySelector('#h4');
 
     // Get Minimum value from input
     let minValue = Number(document.getElementById("minValue").value);
@@ -9,12 +21,31 @@ const randomNumber = () => {
     // Random number generation equation
     let randomNumber = (Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue);
 
-    randomNumber = `The random number between ${minValue} and ${maxValue} is : ${randomNumber}`;
+    randomNumber = `The random number between <span style="color: #ffffff">${minValue}</span> and <span style="color: #ffffff">${maxValue}</span> is : <span style="color: #ffffff">${randomNumber}</span>`;
 
+    result.innerHTML = randomNumber;
     console.log(randomNumber);
 
-    document.querySelector("#h4").innerHTML = randomNumber;
-
-    return;
 }
+
+
+
+
+
+
+
+
+
+
+function getUrl(e) {
+
+
+    // window.location.href: HTML DOM Window.location property returns a Location object that contains information about the current location of the document.
+    const url1 = window.location.href;
+
+    // Document.URL: The DOM URL property in HTML is used to return a string that contains the complete URL of the current document. The string also includes the HTTP protocol such as ( http://).
+    const url2 = document.URL;
+
+
+};
 
